a3fab17d7b2ea9373500c3b6d56c623e88cb2f14
iamsdas <suryashankardas.2002@gmail.com>
2021-01-10 14:04:53 +0530

    add basic score system

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,49 @@
 <template>
-  <div></div>
+  <div>
+    <h3>
+      high score: {{ highScore }}
+      <br />
+      score: {{ score }}
+    </h3>
+  </div>
+  <div>
+    <button @click="increaseScore">Add score</button>
+    <button @click="decreaseScore">Decrease score</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {}
+  components: {},
+  data() {
+    return {
+      score: 0,
+      highScore: 0
+    };
+  },
+  created() {
+    if (!localStorage.storedHighScore) {
+      localStorage.storedHighScore = this.highScore;
+    }
+    this.highScore = localStorage.storedHighScore;
+  },
+  watch: {
+    highScore(newHighScore) {
+      localStorage.storedHighScore = newHighScore;
+    },
+    score(newScore) {
+      if (newScore > this.highScore) this.highScore = newScore;
+    }
+  },
+  methods: {
+    increaseScore() {
+      this.score += 100;
+    },
+    decreaseScore() {
+      this.score -= 150;
+    }
+  }
 };
 </script>
 
